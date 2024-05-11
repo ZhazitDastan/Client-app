af1e6d41a8ed26ec836fbe2e28fd7511e2cf0f55
self86 <abishov.olzhas@gmail.com>
2024-05-11 16:18:34 +0500

    Add jwt-decode dependency and enhance InfoPage tests

--- src/tests/forComponents/InfoPage.test.jsx
+++ src/tests/forComponents/InfoPage.test.jsx
@@ -1,27 +1,37 @@
-import { describ, test } from "vitest";
+import { describe, test } from "vitest";
 import {render, screen, fireEvent} from "@testing-library/react";
 import { BrowserRouter } from 'react-router-dom'
 import "@testing-library/jest-dom/vitest";
 import { InfoPage } from "../../component/info/InfoPage";
 
-/*test('should render the header component', () => {
-    render(
-        <BrowserRouter>
-            <InfoPage />
-        </BrowserRouter>
-    );
-    const headerElement = screen.getByRole('banner');
-    expect(headerElement).toBeInTheDocument();
-});
+// test('should render the header component', () => {
+//     render(
+//         <BrowserRouter>
+//             <InfoPage />
+//         </BrowserRouter>
+//     );
+//     const headerElement = screen.getByRole('banner');
+//     expect(headerElement).toBeInTheDocument();
+// });
 
-test('should redirect to the buynow page when the "BUY NOW" button is clicked', () => {
-    render(
-        <BrowserRouter>
-            <InfoPage />
-        </BrowserRouter>
-    );
-    const buttonElement = screen.getByText('BUY NOW');
-    fireEvent.click(buttonElement);
-    expect(window.location.pathname).toBe('/buynow');
+describe('InfoPage Component Tests', () => {
+    test('test_render_info_page_structure', () => {
+        render(<BrowserRouter><InfoPage /></BrowserRouter>);
+        expect(screen.getAllByText('INFO'));
+        expect(screen.getByText('Here you can information about our project.')).toBeInTheDocument();
+        expect(screen.getByText('PRODUCTS')).toBeInTheDocument();
+        expect(screen.getByText('Our product are high quality works.')).toBeInTheDocument();
+        expect(screen.getByText('BASE')).toBeInTheDocument();
+        expect(screen.getByText('We were founded in 2023 as a team of smart and talented programmers fro SDU.')).toBeInTheDocument();
+        expect(screen.getByText('JOIN OUR TEAM')).toBeInTheDocument();
+        expect(screen.getByText('Buy now')).toBeInTheDocument();
+        expect(screen.getByText('Chose one of our product and enjoy')).toBeInTheDocument();
+    });
+
+    test('test_info_page_image_sources_and_alts', () => {
+        render(<BrowserRouter><InfoPage /></BrowserRouter>);
+        expect(screen.getByAltText('main')).toHaveAttribute('src', expect.stringContaining('info1.png'));
+        expect(screen.getByAltText('mainSdu')).toHaveAttribute('src', expect.stringContaining('main_SDU.png'));
+        expect(screen.getByAltText('SDU')).toHaveAttribute('src', expect.stringContaining('main_LOGO.png'));
+    });
 });
-*/
